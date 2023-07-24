3142c84abc2141da70793ac6c3a1266711638819
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-07-24 04:33:53 -0700

    fixed betty errors

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -11,52 +11,44 @@
  */
 void counting_sort(int *array, size_t size)
 {
-	unsigned int i;
-	int *count = NULL;
-	int *sorted = NULL;
-	unsigned int max = array[0];
+	int *count = NULL, *sorted = NULL;
+	unsigned int i, max = array[0];
 
 	if (array == NULL || size < 2)
 		return;
-    
+
 	for (i = 0; i < size; i++)
 	{
 		if (array[i] >= (int)max)
 		max = array[i];
 	}
-    
+
 	count = malloc(sizeof(int) * (max + 1));
 	sorted = malloc(sizeof(int) * size);
-    
+
 	if (count == NULL || sorted == NULL)
 	{
 		free(count);
 		free(sorted);
 		return;
 	}
-    
 	for (i = 0; i <= max; i++)
 		count[i] = 0;
-    
 	for (i = 0; i < size; i++)
 		count[array[i]]++;
-    
 	for (i = 1; i <= max; i++)
 		count[i] += count[i - 1];
 
 	print_array(count, max + 1);
 
-	for (i = 0; i < size; i++) 
+	for (i = 0; i < size; i++)
 	{
 		sorted[count[array[i]] - 1] = array[i];
 		count[array[i]]--;
 	}
-    
 	for (i = 0; i < size; i++)
-	{
 		array[i] = sorted[i];
-	}
-    
+
 	free(count);
 	free(sorted);
 }
