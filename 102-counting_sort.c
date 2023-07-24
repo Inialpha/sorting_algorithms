7ee6d1076f318d6ab25f9fd3b08e42467ba080c3
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-07-24 03:51:06 -0700

    creats the sorting algorithm function

---
+++ 102-counting_sort.c
@@ -0,0 +1,61 @@
+#include "sort.h"
+#include <stdlib.h>
+
+/**
+ * counting_sort - Sort an array of integers in ascending order
+ *                 using the counting sort algorithm.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ *
+ * Description: Prints the counting array after setting it up.
+ */
+void counting_sort(int *array, size_t size)
+{
+	int *count = NULL
+	int *sorted = NULL
+	int max = array[0];
+
+	if (array == NULL || size < 2)
+		return;
+    
+	for (i = 0; i < size; i++)
+	{
+		if (array[i] > max)
+		max = array[i];
+	}
+    
+	count = malloc(sizeof(int) * (max + 1));
+	sorted = malloc(sizeof(int) * size);
+    
+	if (count == NULL || sorted == NULL)
+	{
+		free(count);
+		free(sorted);
+		return;
+	}
+    
+	for (i = 0; i <= max; i++)
+		count[i] = 0;
+    
+	for (i = 0; i < size; i++)
+		count[array[i]]++;
+    
+	for (i = 1; i <= max; i++)
+		count[i] += count[i - 1];
+
+	print_array(count, max + 1);
+
+	for (i = 0; i < size; i++) 
+	{
+		sorted[count[array[i]] - 1] = array[i];
+		count[array[i]]--;
+	}
+    
+	for (i = 0; i < size; i++)
+	{
+		array[i] = sorted[i];
+	}
+    
+	free(count);
+	free(sorted);
+}
